d222b015cd6da2c84af7c0544ecb1a41c29a5475
Yossab_Samuel <yossabpro5@gmail.com>
2025-01-11 07:35:59 +0200

    add sign out button and welcom section

--- index.php
+++ index.php
@@ -7,22 +7,30 @@
   <title>login System by OOP PHP</title>
 </head>
 <body>
+  <button type="submit" name="signout">signout</button>
   <div class="container">
-    <div class="box signup">
-      <form action="includes/signup.inc.php" method="post">
-        <input type="text" name="uid" placeholder="User name">
-        <input type="password" name="pwd" placeholder="Password">
-        <input type="password" name="pwd" placeholder="Repeat Password">
-        <input type="email" name="email" placeholder="E-mail">
-        <button type="submit" name="submit">signup</button>
-      </form>
+    <div class="welcome-section">
+      <h1>welcome, name</h1>
     </div>
-    <div class="box login">
-      <form action="includes/login.inc.php" method="post">
-        <input type="text" name="uid" placeholder="User name">
-        <input type="password" name="pwd" placeholder="Password">
-        <button type="submit" name="submit">login</button>
-      </form>
+  </div>
+  <div class="container">
+    <div class="section">
+      <div class="box signup">
+        <form action="includes/signup.inc.php" method="post">
+          <input type="text" name="uid" placeholder="User name">
+          <input type="password" name="pwd" placeholder="Password">
+          <input type="password" name="pwd" placeholder="Repeat Password">
+          <input type="email" name="email" placeholder="E-mail">
+          <button type="submit" name="submit">signup</button>
+        </form>
+      </div>
+      <div class="box login">
+        <form action="includes/login.inc.php" method="post">
+          <input type="text" name="uid" placeholder="User name">
+          <input type="password" name="pwd" placeholder="Password">
+          <button type="submit" name="submit">login</button>
+        </form>
+      </div>
     </div>
   </div>
 </body>
